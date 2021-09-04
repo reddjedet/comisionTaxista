a6e8b103c0348b1a855de6ddd6a2dd748ad5ec25
Christian <quidelchristian@gmail.com>
2021-09-04 11:35:52 -0300

    Actualizacion
    Mejorado estética y funcionalmente. Este trabajo está hecho para teléfono (480 max-width).

--- js/taxi.js
+++ js/taxi.js
@@ -1,19 +1,21 @@
-let monto = prompt("Recaudación a ingresar...");
+document.getElementById("btn").addEventListener("click", function () {
+  let monto = prompt("RECAUDACIÓN A INGRESAR...");
 
-function calcular(monto) {
-  if (monto >= 4000) {
-    alert(
-      ` El chofer se lleva el 35%, o sea $${(monto * 0.35).toFixed(
-        2
-      )} \n El titular se lleva el 65%, o sea $${(monto * 0.65).toFixed(2)} `
-    );
-  } else {
-    alert(
-      ` El chofer se lleva el 30%, o sea $${(monto * 0.3).toFixed(
-        2
-      )} \n El titular se lleva el 70%, o sea $${(monto * 0.7).toFixed(2)} `
-    );
+  function calcular(monto) {
+    if (monto >= 4000) {
+      alert(
+        ` El chofer se lleva el 35%, o sea $${(monto * 0.35).toFixed(
+          2
+        )} \n El titular se lleva el 65%, o sea $${(monto * 0.65).toFixed(2)} `
+      );
+    } else {
+      alert(
+        ` El chofer se lleva el 30%, o sea $${(monto * 0.3).toFixed(
+          2
+        )} \n El titular se lleva el 70%, o sea $${(monto * 0.7).toFixed(2)} `
+      );
+    }
   }
-}
 
-calcular(monto);
+  calcular(monto);
+});
